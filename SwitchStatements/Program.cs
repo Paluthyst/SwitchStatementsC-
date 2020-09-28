cce10857b689d4e35a3f93cc4732f043562f84d4
Paluthyst <66975756+Paluthyst@users.noreply.github.com>
2020-09-28 17:45:14 -0400

    Add project files.

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is it the morning, afternoon or night?");
+            String timeOfDay = Console.ReadLine();
+            switch(timeOfDay)
+            {
+                case "morning":
+                    Console.WriteLine("Ah! Good Morning Friend :) ");
+                    break;
+                case "afternoon":
+                    Console.WriteLine("Ah! Good afternoon how has your day been?");
+                    break;
+                case "night":
+                    Console.WriteLine("Good Night I hope you sleep well");
+                    break;
+                default:
+                    Console.WriteLine("Invalad time of day");
+                    break;
+            }
+
+        }
+    }
+}
